515ccfec8c901f5eccf8f7eb7b008c30bf8d4c2f
SaidDemon17 <andreymeowxd@gmail.com>
2026-03-20 16:58:40 -0500

    Update temperatura_controller.py

--- app/controllers/temperatura_controller.py
+++ app/controllers/temperatura_controller.py
@@ -29,13 +29,17 @@ def obtener_datos():
     conn.close()
 
     datos = [{"id": r[0], "valor": r[1]} for r in rows]
-    n=len(datos)
+
+    n = len(datos)
+
+    # Ordenar (ej: mayor a menor)
     for i in range(n - 1):
         for j in range(i + 1, n):
             if datos[i]["valor"] < datos[j]["valor"]:
                 aux = datos[i]
                 datos[i] = datos[j]
                 datos[j] = aux
+
     return {
         "datos": datos
     }
